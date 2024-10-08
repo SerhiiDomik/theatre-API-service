e39da6973d47e8951526ac3944ee37bf7cee35fc
Serhii <serhii.domik@gmail.com>
2024-10-08 14:59:58 +0300

    created models in models.py

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=50)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, blank=True)
+    genres = models.ManyToManyField(Genre, blank=True)
+    # image??
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models)
+    show_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return (self.play.name + " in "
+                + self.theatre_hall.name + " at "
+                + self.show_time)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField()
+    user: models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.created_at } - {self.user.username}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
